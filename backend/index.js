dfe335b73e1f6067ab8f6dd485f3dc72a6897bef
kapuriajeet <32730602+kapuriajeet@users.noreply.github.com>
2022-11-10 21:29:31 +0530

    created models

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
+require('dotenv').config()
+
+
 const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => res.send('Hello World !! I am here to stay'))
+mongoose.connect(process.env.DATABASE)
+    .then(() => {
+    console.log("MongoDB Connected")})
+    .catch(
+        console.log("DB NOT CONNECTED"))
 
 app.listen(port, () => console.log(`Server Started on ${port}`))
